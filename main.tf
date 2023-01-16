e4ce5cb0b7cb3fde1fc74bfa6366bf259547e388
balunagalli <89579055+balunagalli@users.noreply.github.com>
2023-01-16 15:09:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,13 @@ provider "azurerm" {
     features {}
 }
 
-resource "azurerm_resource_group" "test-rg" {
+resource "azurerm_resource_group" "testrg" {
     name = var.rg_name
     location = var.location
     
 }
+
+output "rg-name"{
+   description = "value of resource group"
+   value = azurerm_resource_group.testrg
+ }
